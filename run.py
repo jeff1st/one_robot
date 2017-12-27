2039843246a58c997ebbe4f651a40581af234c23
jeff1st <jeff1st@gmail.com>
2017-12-27 14:18:14 +0100

    simple anwsers added

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from nltk.tag import pos_tag
 HELLOWORDS = [ "Hello", "Hi!", "Hi", "Hey!", "Hey", "Hola!", "Hola", "How are you!" ]
 BYEWORDS = [ "See you.", "Take care.", "Nice day!", "Bye!", "BR" ]
 FINEWORDS = [ "I'm fine thank you", "Fine and you?", "Very well", "Fine, thank you!", "I'm ok, hope you're ok too" ]
+STATEVERBS = [ "be", "feel", "seem", "stay" ]
 
 class Robot:
     def __init__(self):
@@ -38,14 +39,25 @@ class Robot:
 
     def comprehension(self, list_of_sent):
         results = []
+        returned_sent = ""
         for sentence in list_of_sent:
             results.append(pos_tag(sentence))
+        print results
 
         for result in results:
+            temp = { k[0]:v[1] for k, v in zip(result, result) }
+            print temp
+            
             for (word, pos) in result:
                 if word.title() in HELLOWORDS:
-                    print HELLOWORDS[random.randint(0, 4)]
+                    returned_sent += (HELLOWORDS[random.randint(0, len(HELLOWORDS)-1)] + ". ")
 
-        
+            if 'you' in temp.keys():
+                if temp['you']=='PRP':
+                    if 'WRB' in temp.values() and 'PRP' in temp.values():
+                        returned_sent += (FINEWORDS[random.randint(0, len(FINEWORDS)-1)] + ". ")
+
+        returned_sent.strip()
+        print returned_sent
 
 robot = Robot()
